eecd4fd1291c67179420e0214f67fbb81a2e79b4
jaeeun-rapportlabs <jaeeun@rapportlabs.kr>
2023-04-20 15:35:36 +0900

    1.0.8

--- packages/mobile-web/build/rollup.config.bundle.js
+++ packages/mobile-web/build/rollup.config.bundle.js
@@ -2,14 +2,14 @@ import babel from '@rollup/plugin-babel';
 import { resolve } from 'path';
 import copy from 'rollup-plugin-copy';
 import url from '@rollup/plugin-url'
-import multi from '@rollup/plugin-multi-entry'
+import generatePackageJson from "rollup-plugin-generate-package-json";
 
 export default [
   {
     input: 'src/png/index.ts',
     output: {
       dir: 'dist/png',
-      format: 'es',
+      format: 'esm',
     },
     plugins: [
       url(),
@@ -17,6 +17,14 @@ export default [
         targets: [
           { src: resolve('src/png/index.d.ts'), dest: resolve('dist/png') },
         ]
+      }),
+      generatePackageJson({
+        baseContents: {
+          name: `damoa-assets-mobile-web/png`,
+          private: true,
+          module: "./index.ts", // --> points to esm format entry point of individual component
+          types: "./index.d.ts", // --> points to types definition file of individual component
+        },
       }),
       babel({
         exclude: 'node_modules/**',
@@ -27,7 +35,7 @@ export default [
     input: 'src/svg/index.ts',
     output: {
       dir: 'dist/svg',
-      format: 'es',
+      format: 'esm',
     },
     plugins: [
       url(),
@@ -35,6 +43,14 @@ export default [
         targets: [
           { src: resolve('src/svg/index.d.ts'), dest: resolve('dist/svg') },
         ]
+      }),
+      generatePackageJson({
+        baseContents: {
+          name: `damoa-assets-mobile-web/svg`,
+          private: true,
+          module: "./index.ts", // --> points to esm format entry point of individual component
+          types: "./index.d.ts", // --> points to types definition file of individual component
+        },
       }),
       babel({
         exclude: 'node_modules/**',
